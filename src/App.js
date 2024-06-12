17899f48a37254b608af1c71fc8f72a5a75164ce
grberrett <grberrett@gmail.com>
2024-06-11 20:13:37 -0700

    fixed typo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Does this update?</p>>
+        <p>Does this update?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
